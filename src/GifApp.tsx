6f631147652c6dab41795f602fbb89a2a61287d0
Sergio Mir <voidtracer6@gmail.com>
2023-06-17 14:09:13 -0400

    ./scr/GiffApp.tsx: new main component

---
+++ src/GifApp.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategoryComponent/AddCategory.tsx';
+
+export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['Dragon Ball', 'The Boys', 'Breaking Bad']);
+
+
+  // const handleAddCategory = () => {
+  //   const newCategory: string = 'Thing 1';
+  //   setCategories( categories.concat( newCategory ) );
+  // }
+
+  return (
+    <>
+      <h2> GifApp </h2>
+      <AddCategory />
+      <hr />
+
+      <ol>
+        {
+          categories.map( ( category: string ) => {
+            return <li key= { category }> { category } </li>
+          } )
+        }
+      </ol>
+    </>
+  );
+}
---
+++ src/components/AddCategoryComponent/AddCategory.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "../../styles/AddCategory.css"
+
+export const AddCategory = () => {
+  return (
+    <>
+    <input type='text'></input>
+    </>
+  )
+}
