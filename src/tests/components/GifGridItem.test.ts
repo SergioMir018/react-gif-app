e27b40d5748529cf2843a390d53fefab7ab8c2b5
Sergio Mir <voidtracer6@gmail.com>
2023-06-20 02:25:14 -0400

    ./scr/tests/components/GifGridItem.test.ts: starting the implementation of the tests

---
+++ src/tests/components/GifGridItem.test.ts
@@ -0,0 +1,19 @@
+import React from "react";
+import { describe } from "node:test";
+import { shallow } from 'enzyme';
+
+
+import { GifGridItem } from '../../components/GifGridComponent/GifGridItemComponent/GifGridItem';
+
+
+describe('Tests in <GifGridItem />', () => {
+
+
+  test('should show the component correctly', () => {
+    
+    const wrapper = shallow( <GifGridItem/>  )  
+
+  })
+  
+
+});
